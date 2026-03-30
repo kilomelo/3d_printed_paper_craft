6f9626c7b41680a38625ce8a910a7f37546661e6
chenweichu <weichuyoucai@gmail.com>
2026-03-30 23:07:17 +0800

    补充叠色工具页面的视频链接，打开视频开关

--- src/modules/luminaLayers/luminaLayersConfig.ts
+++ src/modules/luminaLayers/luminaLayersConfig.ts
@@ -1,6 +1,6 @@
 // Lumina-Layers 工具的页面级配置。
 // 发布视频前可保持 false，仅显示占位；视频就绪后改为 true 即可启用内嵌播放器。
-export const LUMINA_LAYERS_EMBEDDED_VIDEO_ENABLED = false;
+export const LUMINA_LAYERS_EMBEDDED_VIDEO_ENABLED = true;
 
 export const LUMINA_LAYERS_EMBEDDED_VIDEO_SRC =
-  "//player.bilibili.com/player.html?isOutside=true&aid=116085248493480&bvid=BV1cAZkBeEZ6&cid=36120365789&p=1&t=0&autoplay=0";
+  "//player.bilibili.com/player.html?isOutside=true&bvid=BV1NDXQBRE9L&p=2&t=2&autoplay=0";
